fc96ee8dd710aed3cd7f8aa79ce351911488d8bc
Web Trade Consulting <dev@webtradeconsulting.com>
2017-03-11 14:36:12 +0600

    Update wp-config.php

--- wp-config.php
+++ wp-config.php
@@ -20,13 +20,13 @@
 
 // ** MySQL settings - You can get this info from your web host ** //
 /** The name of the database for WordPress */
-define('DB_NAME', 'wks_zen');
+define('DB_NAME', 'zen');
 
 /** MySQL database username */
-define('DB_USER', 'wks_zen');
+define('DB_USER', 'root');
 
 /** MySQL database password */
-define('DB_PASSWORD', 'ictvm9Ercu');
+define('DB_PASSWORD', 'root');
 
 /** MySQL hostname */
 define('DB_HOST', 'localhost');
